a27f391dc5173d85aceb2630c36ed70d64992a10
D-Bhatta <dbhatta1232@gmail.com>
2020-07-18 01:46:07 -0400

    Add files for Exercise: Binomial Class
    4a_binomial_package.tar.gz:
      - Add zip file for the exercise
    
    Binomialdistribution.py:
      - Add exercise file with some pre-written code
    
    Binomialdistribution_challenge.py:
      - Add exercise file
    
    distributions/:
      - Add package from previous exercise
    
    numbers_binomial.txt:
      - Add dataset for the exercise
    
    numbers.txt:
      - Add dataset for distributions package
    
    setup.py:
      - Add setup file for the Binomial package
    
    test.py:
      -Add unit tests for the package

---
+++ packages/4a_binomial_package/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="distributions",
+    version="0.1",
+    description="Gaussian distributions",
+    packages=["distributions"],
+    zip_safe=False,
+)
